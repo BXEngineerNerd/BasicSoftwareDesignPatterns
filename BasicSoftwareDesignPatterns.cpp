07be0f8702bef6c1dd1b5967de90c95af118a525
War_Waging_Hippie <manmayhemmusic.com>
2023-05-20 11:02:09 -0700

    Add project files.

---
+++ BasicSoftwareDesignPatterns.cpp
@@ -0,0 +1,12 @@
+﻿// BasicSoftwareDesignPatterns.cpp : Defines the entry point for the application.
+//
+
+#include "BasicSoftwareDesignPatterns.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
